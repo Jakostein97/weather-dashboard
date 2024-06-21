b820db4d01375ba8922277e1e90b6d5e4eecb86c
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-06-21 15:28:45 -0400

    Adds data to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,31 @@
 const APIKey = "ee0f05e96c33243a6586d6b864f507ae";
 let city;
 const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
+const mainWeather = document.querySelector('.main-card')
 // Functions
 function search() {
 
     city = (document.getElementById("search-city").value) || 'New York';
-    const currentWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    const currentWeatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 
     fetch(currentWeatherURL)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
+            const todayDate = new Date(data.dt * 1000).toLocaleDateString()
+            const code = data.weather[0].icon
+            const url = `https://openweathermap.org/img/wn/${code}.png`
+            console.log(todayDate);
+            const weatherCard = `
+            <div class="card-body">
+                  <h5 class="card-title">${data.name} ${todayDate}</h5>
+                  <img src="${url}"/> 
+                  <p class="card-text">Temperature: ${data.main.temp}F</p>
+                  <p class="card-text">Wind: ${data.wind.speed}mph</p>
+                  <p class="card-text">Humidity: ${data.main.humidity}%</p>
+                </div>
+            `;
+            mainWeather.innerHTML = weatherCard
             const lat = data.coord.lat;
             const lon = data.coord.lon;
             const forecastURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
